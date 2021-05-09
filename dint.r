abeecb6a8b474ade6849dba92ea4db30138966c5
Reza Norouzian <rnorouzian@gmail.com>
2021-05-09 12:36:18 -0500

    Update dint.r

--- dint.r
+++ dint.r
@@ -95,8 +95,8 @@ rdif <- function(n = NA, mpre = NA, mpos = NA, sdpre = NA, sdpos = NA, t.pair = 
   
   ifelse(!is.na(sdif) & is.na(sdp) & !is.na(sdpre) & !is.na(sdpos), (sdpre^2 + sdpos^2 - sdif^2)/(2*sdpre*sdpos), 
          ifelse(!is.na(sdp) & !is.na(sdif), 1 - (sdif^2/(2*sdp^2)), NA))
-}
-
+}           
+           
 #===============================================================================================================================
 
 cfactor <- function(df) exp(lgamma(df/2)-log(sqrt(df/2)) - lgamma((df-1)/2))
@@ -461,4 +461,33 @@ Type_of_Input_ <- c(
   "numeric [0-1]", "logical","logical","logical",
   "numeric","numeric","numeric",
   "numeric","logical",
-  "numeric", "numeric/character")                         
+  "numeric", "numeric/character")        
+                         
+#============================================================================================================================================
+                         
+mask_ <- function(data, what, full = FALSE){
+  
+  data[] <- lapply(data, function(x) type.convert(as.character(x), as.is = TRUE))
+  
+  f1 <- function(x) as.numeric(factor(x, levels = unique(x)))
+  f2 <- function(x) {
+    temp <- substr(x, 1, 1)
+    paste0(temp, ave(x, temp, FUN = function(y) match(y, unique(y))))
+  }
+  cols <- names(data)[sapply(data, is.numeric)]
+  num.cols <- cols[cols %in% what]
+  cols <- names(data)[sapply(data, is.character)]
+  char.cols <- cols[cols %in% what]
+  
+  if(!full){
+    
+    if(length(num.cols))  data[num.cols] <- lapply(data[num.cols], f1)
+    if(length(char.cols)) data[char.cols] <- lapply(data[char.cols], f2)
+    
+  }else{
+    
+    data[what] <- lapply(data[what], f1)
+  }
+  return(data)
+}
+                         
